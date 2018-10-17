66503eb580c06673a753386768f55276ced4179d
Jonas Salen <jonassalen@gmail.com>
2018-10-17 18:29:18 +0200

    wijzigingen PJ

--- init.js
+++ init.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
 
   $('[data-goto]').on('click', function(e) {
     var slide = $(this).data('goto');
+    console.log(slide);
     if(isNaN(slide)) {
       $('.message[data-message=' + slide + ']').addClass('is-open');
       $('.keuzewijzer').addClass('is-disabled');
     } else {
-      $('.keuzewijzer').css('transform', 'translateX(-' + (slide-1)*10 + '%)');
+      $('.keuzewijzer').css('transform', 'translateX(-' + slide*10 + '%)');
     }
     e.preventDefault();
   });
